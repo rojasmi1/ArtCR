e3801a79b439734d54484b7aef3e94351d0a0017
Michael Rojas <michael.rojasp@gmail.com>
2016-09-07 18:19:01 +0000

    Add navigation menu items
    All the website items were added without any links.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,13 +3,34 @@ import { IndexLink, Link } from 'react-router'
 import classes from './Header.scss'
 
 export const Header = () => (
-  <nav className={`navbar ${classes.navbarBlue} navbar-fixed-top`}>
-    <div className="container">
-      <div className="nav-header">
-        <a href="#" id="brand-name" className="navbar-brand">Max Rojas Vargas</a>
-      </div>
+<nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand" href="#">Max Rojas Vargas!</a>
     </div>
-  </nav>
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav">
+        <li><a href="#">Home</a></li>
+        <li className="dropdown">
+          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Meet the artist<span className="caret"></span></a>
+          <ul className="dropdown-menu">
+            <li><a href="#">About Max</a></li>
+            <li><a href="#">About the studio</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">Shows</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 )
 
 export default Header
