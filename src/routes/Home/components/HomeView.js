a75793ba05624ba3b8e2b8d4d722576e21df8ecb
Michael Rojas <michael.rojasp@gmail.com>
2016-10-24 22:25:30 -0600

    Use WelcomeMessage component inside Home view.

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import classes from './HomeView.scss'
 import MaxImage from '../assets/maxrojas-home-image.jpg'
+import WelcomeMessage from '../../../components/WelcomeMessage'
+import * as Messages from '../../../static/messages_en'
 
 export const HomeView = () => (
   <section id="about-the-author" className="row">
@@ -9,14 +11,7 @@ export const HomeView = () => (
     <img alt='Max Rojas Vargas' className="img-responsive img-rounded" src={MaxImage} />
   </div>
   <div className="col-md-5 col-lg-5">
-    <p>
-      <strong>Max Rojas Vargas</strong> was born and raised in a rural farming village in the province of Cartago, Costa Rica.
-      His formal studies Max received at the University of Costa Rica...thus beginning his immersion into the world of art.
-      Rojas, one of Costa Rica's established watercolorists, immerses himself in his environment and transposes this experience to paper or canvas.
-      The viewer is able to feel the humidity of the swollen clouds, the crispness of  the first steam of light after the storm breaks, swirling hues caught in nature's phenomena.
-      Max's acrylics are vigorous, energy-charged renditions conscious of space, surface and color of Costa Rica.
-      Rojas also forges metal, carves in wood and weaves on looms he builds himself; total corporal involvement that creates art full of activity, be it conceptual or physical.
-    </p>
+    <WelcomeMessage painter={Messages.welcomeMessageAuthor} message={Messages.welcomeMessageBody}/>
   </div>
   </section>
 )
