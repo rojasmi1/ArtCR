6b89bc9ff6ef987b9dcb25792eebccca732bb47c
Michael Rojas <michael.rojasp@gmail.com>
2016-10-24 22:30:54 -0600

    Move ´about the author´ css to the Home view

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -5,7 +5,7 @@ import WelcomeMessage from '../../../components/WelcomeMessage'
 import * as Messages from '../../../static/messages_en'
 
 export const HomeView = () => (
-  <section id="about-the-author" className="row">
+  <section id="about-the-author" className={`row ${classes.aboutTheAuthor}`}>
   <header className={classes.mainHeader}>About Max Rojas</header>
   <div className="col-md-5 col-md-offset-1 col-lg-5 col-lg-offset-1">
     <img alt='Max Rojas Vargas' className="img-responsive img-rounded" src={MaxImage} />
