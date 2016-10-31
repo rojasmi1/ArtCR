77c01edfa1ce46b4f2ad9e0563d0ca4e36c7750b
Michael Rojas <michael.rojasp@gmail.com>
2016-10-30 20:16:03 -0600

    Separete text from the component in the Footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { IndexLink, Link } from 'react-router'
 import classes from './Footer.scss'
 
-export const Footer = () => (
+export const Footer = (props) => (
   <footer className={classes.footer}>
     <div className="container">
       <section className="row" id="copyright">
-          <span className="text-muted">Copyright © 2016 Michael Rojas.</span>
+          <div className="col-lg-6">
+            <span className="text-muted" dangerouslySetInnerHTML={props.text()}></span>
+          </div>
+          <div className="col-lg-4 col-lg-offset-2">
+
+          </div>
       </section>
     </div>
   </footer>
--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import classes from './CoreLayout.scss'
+import * as Messages from '../../static/messages_en'
 import '../../styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div>
     <Header menuId="mainMenu"/>
       {children}
-    <Footer />
+    <Footer text={Messages.footerText}/>
   </div>
 )
 
