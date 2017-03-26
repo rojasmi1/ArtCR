e79d98d8f00777c9473ce157f19980066a0bc59d
Michael Rojas <michael.rojasp@gmail.com>
2017-03-26 01:53:05 -0600

    Implement PreviewNavigationBox layout and animations.

--- src/components/PreviewNavigationBox/PreviewNavigationBox.js
+++ src/components/PreviewNavigationBox/PreviewNavigationBox.js
@@ -3,14 +3,14 @@ import classes from './PreviewNavigationBox.scss'
 
 export const PreviewNavigationBox = (props) => (
 // <p> <strong>{props.painter}</strong>  <span dangerouslySetInnerHTML={props.message()}/></p>
-<article className="${classes.navigationBox}">
+<article className={classes.navigationBox}>
 <div className={classes.headerWrapper}>
   <h2>{props.title}</h2>
 </div>
-<section className="${classes.boxContent}">
+<section className={classes.boxContent}>
 {props.content}
 </section>
-<a href={props.readMore.link} target="_blank" className="${classes.readMore}">{props.readMore.text}</a>
+<a href={props.readMore.link} target="_blank" className={classes.readMore}>{props.readMore.text}</a>
 </article>
 )
 
